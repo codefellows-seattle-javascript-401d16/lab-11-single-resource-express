3d14c6270934009efa2f71a90238d5067328f922
Ryan Felix <therjfelix@gmail.com>
2017-06-26 16:32:23 -0700

    build out routes

--- lab-ryan-felix/route/sofa-route.js
+++ lab-ryan-felix/route/sofa-route.js
@@ -11,12 +11,38 @@ sofaRouter.post('/api/sofas', jsonParser, (req, res) => {
 
   new Sofa(req.body)
     .save()
-    .then(sofa => res.json(sofa))
+    .then(sofa => res.status(201).json(sofa))
     .catch(err => {
       res.status(400)
          .send(err);
     });
+});
 
+sofaRouter.get('/api/sofas/:id', (req, res) => {
+
+  console.log('Received GET to /api/sofas/:id');
+
+  Sofa.findById(req.params.id)
+    .then(sofa => res.status(200).json(sofa))
+    .catch(() => res.status(404).send());
+});
+
+sofaRouter.put('/api/sofas/:id', (req, res) => {
+
+  console.log('Received PUT to /api/sofas/:id');
+
+  Sofa.findByIdAndUpdate(req.params.id, req.body)
+    .then(sofa => res.status(202).json(sofa))
+    .catch(() => res.status(404).send());
+});
+
+sofaRouter.delete('/api/sofas/:id', (req, res) => {
+
+  console.log('Received DELETE to /api/sofas/:id');
+
+  Sofa.findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).send())
+    .catch(() => res.status(404).send());
 });
 
 module.exports = sofaRouter;
