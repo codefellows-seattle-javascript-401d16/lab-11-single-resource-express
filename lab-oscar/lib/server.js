d075c47e0ffcde04b17c27b4c5cfb40f141c9df5
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-26 16:34:28 -0700

    server is running

---
+++ lab-oscar/lib/server.js
@@ -0,0 +1,35 @@
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+let server;
+const app = express();
+
+app.get('/api/hello', (req, res, next) => {
+  res.send('hello World');
+});
+
+const serverControl = module.exports = {};
+
+serverControl.start = () => {
+  return new Promise((resolve) => {
+    server = app.listen(process.env.PORT, () => {
+      console.log('server is running', process.env.PORT);
+      server.isOn = true;
+      resolve();
+    });
+  });
+};
+
+serverControl.stop = () => {
+  return new Promise((resolve) => {
+    server.close(() => {
+      console.log('server is down');
+      server.isOn = false;
+      resolve();
+    });
+  });
+};
