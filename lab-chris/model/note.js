a21dfeed885b2c086307cb3185f576004e10d4d4
janousek77 <janousek77@gmail.com>
2017-06-26 21:52:51 -0700

    Got demo code up and running. Added put method and test using demo code. Both work

---
+++ lab-chris/model/note.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+
+const noteSchema = mongoose.Schema({
+  content: {type:String, required: true},
+  created: {type:Date, required: true},
+});
+
+
+module.exports = mongoose.model('note', noteSchema);
---
+++ lab-chris/route/router.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const Note = require('../model/note.js');
+
+let noteRouter = module.exports = new Router();
+
+noteRouter.post('/api/notes', jsonParser, (req, res, next) => {
+
+  req.body.created = new Date();
+
+  new Note(req.body)
+    .save()
+    .then(note => res.json(note))
+    .catch(next);
+});
+
+noteRouter.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => res.json(note))
+    .catch(next);
+});
+
+noteRouter.put('/api/notes/:id', jsonParser, (req, res, next) => {
+
+  Note.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(note => res.json(note))
+    .catch(next);
+});
+
+noteRouter.delete('/api/notes/:id', (req, res, next) => {
+
+  Note.findByIdAndRemove(req.params.id)
+    .then(() => res.write(`${req.params.id} deleted`))
+    .catch(next);
+});
