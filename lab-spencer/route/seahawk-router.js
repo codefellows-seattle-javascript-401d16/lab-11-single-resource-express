6266e05ccc0398ad77a50ef17d09e1de71faac6e
Spencer Gietzen <Speng1516@comcast.net>
2017-06-26 17:23:12 -0700

    wrote the other routes and tests

--- lab-spencer/route/seahawk-router.js
+++ lab-spencer/route/seahawk-router.js
@@ -7,17 +7,32 @@ const Seahawk = require('../model/seahawk.js');
 let seahawkRouter = module.exports = new Router();
 
 seahawkRouter.post('/api/seahawks', jsonParser, (req, res, next) => {
-  console.log('Hit /api/seahawks');
-
   new Seahawk(req.body)
     .save()
     .then(seahawk => res.json(seahawk))
     .catch(next);
 });
 
-seahawkRouter.get('/api/seahawks/:id', jsonParser, (req, res, next) => {
-  console.log('Hit get /api/seahawks/:id');
+seahawkRouter.get('/api/seahawks', (req, res, next) => {
+  Seahawk.find({})
+    .then(seahawk => res.json(seahawk))
+    .catch(next);
+});
+
+seahawkRouter.get('/api/seahawks/:id', (req, res, next) => {
   Seahawk.findById(req.params.id)
     .then(seahawk => res.json(seahawk))
+    .catch(next);
+});
+
+seahawkRouter.put('/api/seahawks/:id', jsonParser, (req, res, next) => {
+  Seahawk.update({_id: req.params.id}, {name: req.body.name, height: req.body.height, weight: req.body.weight, position: req.body.position, picture: req.body.picture})
+    .then(seahawk => res.json(seahawk))
+    .catch(next);
+});
+
+seahawkRouter.delete('/api/seahawks/:id', (req, res, next) => {
+  Seahawk.remove({_id: req.params.id})
+    .then()
     .catch(next);
 });
