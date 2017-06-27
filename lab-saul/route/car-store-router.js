dc7d1abd3c96bdcd07f9d02e5a657920d120555a
Saul Greene <spdysg@gmail.com>
2017-06-26 18:26:22 -0700

    got tests and routes set up still need to handle errs

---
+++ lab-saul/route/car-store-router.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const Car = require('../model/car-store.js');
+
+let carRouter = module.exports = new Router();
+
+carRouter.post('/api/cars', jsonParser, (req, res, next) => {
+  console.log('hit /api/cars');
+
+
+  new Car(req.body)
+  .save()
+  .then(car => res.json(car))
+  .catch(next);
+});
+
+carRouter.get('/api/cars/:id', (req, res, next) => {
+  console.log('hit get /api/cars/:id');
+
+  Car.findById(req.params.id)
+  .then(car => res.json(car))
+  .catch(next);
+});
+
+carRouter.put('/api/cars/:id', jsonParser, (req, res, next) =>{
+  console.log('hit put /api/cars');
+
+  Car.findOneAndUpdate(req.params.id, req.body,{new: true})
+  .then(car => res.json(car))
+  .catch(next);
+});
+
+carRouter.delete('/api/cars/:id', jsonParser, (req, res, next) => {
+  console.log('hit delete /api/cars');
+  console.log(req.params.id);
+  Car.findByIdAndRemove(req.params.id)
+  .then(car => {
+    console.log(car);
+    res.json(car);
+  })
+  .catch(next);
+});
