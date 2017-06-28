e34b0ae80ca7e82caedf7364ca1de3e9d84d536f
adan.rabadan <evilb4k4@gmail.com>
2017-06-28 15:04:54 -0700

    fixed my test and ready to re-submit

--- lab-adan/route/game-router.js
+++ lab-adan/route/game-router.js
@@ -11,29 +11,29 @@ gameRouter.post('/api/gamescore', jsonParser, (req, res, next) => {
   req.body.created = new Date();
 
   new GameScore(req.body)
-  .save()
-  .then(gamescore => res.json(gamescore))
-  .catch(next);
+    .save()
+    .then(gamescore => res.json(gamescore))
+    .catch(next);
 });
 gameRouter.get('/api/gamescore',(req, res, next) => {
   GameScore.find({})
-  .then(gamescore => res.json(gamescore))
-  .catch(next);
+    .then(gamescore => res.json(gamescore))
+    .catch(next);
 
 });
 gameRouter.get('/api/gamescore/:id', (req, res, next) => {
   console.log('hit get /api/gamescore/:id');
   GameScore.findById(req.params.id)
-  .then(gamescore => res.json(gamescore))
-  .catch(next);
+    .then(gamescore => res.json(gamescore))
+    .catch(next);
 });
-gameRouter.put('/api/game/:id', jsonParser, (req, res, next) => {
+gameRouter.put('/api/gamescore/:id', jsonParser, (req, res, next) => {
   GameScore.update({_id: req.params.id}, {name: req.body.name, score: req.body.score})
     .then(() => res.json(req.body)) // because update doesnt return the updated record
     .catch(next);
 });
 
-gameRouter.delete('/api/game/:id', (req, res, next) => {
+gameRouter.delete('/api/gamescore/:id', (req, res, next) => {
   GameScore.deleteOne({_id: req.params.id})
     .then(() => res.sendStatus(200))
     .catch(next);
